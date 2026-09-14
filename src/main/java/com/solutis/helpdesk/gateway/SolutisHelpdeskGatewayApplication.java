0319ae5b9d896662bad09e5eced0aadfb304ecb9
Caio Viana Teixeira Carmo <caio.carmo@solutis.com.br>
2026-09-14 15:58:15 -0300

    Do setup Gateway

---
+++ src/main/java/com/solutis/helpdesk/gateway/SolutisHelpdeskGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.solutis.helpdesk.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class SolutisHelpdeskGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolutisHelpdeskGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solutis/helpdesk/gateway/SolutisHelpdeskGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solutis.helpdesk.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolutisHelpdeskGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
